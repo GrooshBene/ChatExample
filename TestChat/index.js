f3e0acf2feb9b85a7dadeacd221a67f43740e960
GrooshBene <wltn000129@gmail.com>
2015-09-06 14:48:19 +0900

    Port Changed Again to 80

--- TestChat/index.js
+++ TestChat/index.js
@@ -17,6 +17,6 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(8000, function() {
-  console.log('listening on *:8000');
+http.listen(80, function() {
+  console.log('listening on *:80');
 });
